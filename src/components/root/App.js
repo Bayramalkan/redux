53d921f1ca2685343ae36b41700198530f5c60a3
Bayram Alkan <bayramalkan@ogrenci.beykoz.edu.tr>
2021-12-04 00:25:32 +0300

    first commit

---
+++ src/components/root/App.js
@@ -0,0 +1,20 @@
+import { Container } from "reactstrap";
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
+import{Route, Routes} from "react-router-dom"
+import CartDetail from "../cart/CartDetail";
+
+function App() {
+  return (
+    <Container>
+      <Navi />
+      <Routes>
+        <Route path="/" exact element={<Dashboard/>}></Route>
+        <Route path="/cart" exact element={<CartDetail/>}></Route>
+      </Routes>
+      
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from "redux";
+import categoryListReducer from "./categoryListReducer";
+import changeCategoryReducer from "./changeCategoryReducer";
+import productListReducer from "./productListReducer";
+import cartReducer from "./cartReducer"
+
+const rootReducer = combineReducers({
+    //changeCategoryReducer:changeCategoryReducer
+    changeCategoryReducer,
+    categoryListReducer,
+    productListReducer,
+    cartReducer,
+})
+
+
+export default rootReducer;
